135d47895c5afb805c2a2c23baa2d3fcc61b55ba
Zev Goldberg <zevgoldberg@gmail.com>
2015-03-31 00:46:33 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function(angular) {
+    'use strict';
+    angular.module('bc', [])
+    .controller('Controller', ['$scope', '$filter', function($scope, $filter) {
+        $scope.boozes = [];
+        $scope.addBooze = function() {
+            $scope.boozes.push({});
+        };
+        $scope.addBooze();
+        $scope.sortBooze = function() {
+            $scope.boozes = $filter('orderBy')(
+                $filter('filter')($scope.boozes, function(element){
+                    if (element.volume && element.proof) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }), $scope.getDollarPerProof);
+        }
+        $scope.getDollarPerProof = function(booze) {
+            var dollarPerLiter = (booze.cost / booze.volume) * 1000;
+            var dollarPerProof = dollarPerLiter / booze.proof
+            return dollarPerProof;
+        }
+    }]);
+})(window.angular);
